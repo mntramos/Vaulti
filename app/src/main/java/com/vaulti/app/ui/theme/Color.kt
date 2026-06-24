0d9f6c2b8e90963bd9ae0f873eae192f7bd8f012
Mark Ramos <mntramos8@gmail.com>
2026-06-24 15:13:06 +0800

    refactor: remove unused template color constants
    Purple80, PurpleGrey80, Pink80, Purple40, PurpleGrey40, and Pink40
    were default Compose template colors never referenced anywhere.

--- app/src/main/java/com/vaulti/app/ui/theme/Color.kt
+++ app/src/main/java/com/vaulti/app/ui/theme/Color.kt
@@ -2,14 +2,6 @@ package com.vaulti.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val VaultiPrimary = Color(0xFF6C63FF)
 val VaultiSecondary = Color(0xFF03DAC6)
 val VaultiBackground = Color(0xFFF5F5FA)
